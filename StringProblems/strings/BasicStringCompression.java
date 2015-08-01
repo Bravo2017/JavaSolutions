0221b2e33e1c39774d35c2630830e3906e81e606
tanmay <tanmayss@gmail.com>
2015-08-01 17:22:51 -0400

    Compress a string
    Check length of compressed string, return if length of compressed
    string is less

---
+++ StringProblems/strings/BasicStringCompression.java
@@ -0,0 +1,70 @@
+package strings;
+
+public class BasicStringCompression {
+
+	public static void main(String[] args) {
+		BasicStringCompression app = new BasicStringCompression();
+		String input = "aaaabbbcccaaddeee";
+		String compress = app.compressString(input);
+		System.out.println(compress);
+	}
+
+	// compress string
+	public String compressString(String input) {
+
+		int len = input.length();
+		int size = compressLength(input);
+		if (size >= len)
+			return input;
+
+		char[] array = new char[size];
+		int index = 0;
+		char lastSeen = input.charAt(0);
+		int count = 1;
+		for (int i = 1; i < len; ++i) {
+			if (lastSeen == input.charAt(i)) {
+				++count;
+			} else {
+				index = insertIntoArray(index, array, count, lastSeen);
+				lastSeen = input.charAt(i);
+				count = 1;
+			}
+		}
+		index = insertIntoArray(index, array, count, lastSeen);
+		return new String(array);
+	}
+
+	int insertIntoArray(int index, char[] array, int count, char lastSeen) {
+		array[index++] = lastSeen;
+		String numInStr = String.valueOf(count);
+		for (int i = 0; i < numInStr.length(); ++i) {
+			array[index++] = numInStr.charAt(i);
+		}
+		return index;
+	}
+
+	/**
+	 * find the length of the compressed string mathematically without creating
+	 * the String first
+	 */
+	int compressLength(String str) {
+		int size = 0;
+		char lastSeen = str.charAt(0);
+		int count = 1; // one character seen, start from next char
+
+		for (int i = 1; i < str.length(); ++i) {
+			if (lastSeen == str.charAt(i))
+				++count; // increment if ith char is same as last seen
+			else {
+				size += 1; // for the letter
+				// convert count to string and get length- for multidigit count
+				size += String.valueOf(count).length();// 
+				lastSeen = str.charAt(i);// update last seen
+				count = 1; // reset count to 1.
+			}
+		}
+		size += 1 + String.valueOf(count).length();
+		return size;
+	}
+
+}
