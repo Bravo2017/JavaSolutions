bfb5137b533d55b3f9ffcaf498a2e62a72df9c33
tanmay <tanmayss@gmail.com>
2015-08-01 22:28:41 -0400

    Using static inner classes to implement different Comparator classes

---
+++ solutions/comparable/ex3/Person.java
@@ -0,0 +1,88 @@
+package comparable.ex3;
+
+import java.util.Comparator;
+
+/**
+ * Instead of writing separate classes for each comparator implementation
+ * 
+ * 
+ * 
+ * 
+ * */
+class Person implements Comparable<Person> {
+
+	private String firstName;
+	private String lastName;
+	private int age;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public int compareTo(Person o) throws ClassCastException, NullPointerException {
+		if(o == null)	throw new NullPointerException("Null Object passed");
+		if (!(o instanceof Person))
+			throw new ClassCastException("A Person object expected.");
+		int anotherPersonAge = ((Person) o).getAge();
+		return this.age - anotherPersonAge;
+	}
+	
+	@Override
+	public String toString(){
+		return String.format(lastName + ", " + firstName + ". Age:" + age);
+	}
+	
+	//Anonymous class
+	public static class LastNameComparator implements Comparator<Person>{
+		public int compare(Person o1, Person o2) {
+			String lastName1 = o1.getLastName().toUpperCase();
+			String firstName1 = o1.getFirstName().toUpperCase();
+			String lastName2 = o2.getLastName().toUpperCase();
+			String firstName2 = o2.getFirstName().toUpperCase();
+			
+			// if last names are not equal, compare last names
+			// if last names are equal, then sort wrt to first names
+			if (!(lastName1.equals(lastName2)))
+				return lastName1.compareTo(lastName2);
+			else
+				return firstName1.compareTo(firstName2);
+		}
+	}
+	
+	public static class FirstNameComparator implements Comparator<Person>{
+		public int compare(Person o1, Person o2) {
+			String fname1 = o1.getFirstName().toUpperCase();
+			String lname1 = o1.getFirstName().toUpperCase();
+			String fname2 = o2.getFirstName().toUpperCase();
+			String lname2 = o2.getLastName().toUpperCase();
+			
+			if (!(fname1.equals(fname2))) {
+				return fname1.compareTo(fname2);
+			} else {
+				return lname1.compareTo(lname2);
+			}
+		}
+	}
+	
+}
---
+++ solutions/comparable/ex3/Testing.java
@@ -0,0 +1,92 @@
+package comparable.ex3;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class Testing {
+	public static void main(String[] args) {
+		Person[] persons = new Person[4];
+
+		persons[0] = new Person();
+		persons[0].setFirstName("Elvis");
+		persons[0].setLastName("Goodyear");
+		persons[0].setAge(56);
+
+		persons[1] = new Person();
+		persons[1].setFirstName("Stanley");
+		persons[1].setLastName("Clark");
+		persons[1].setAge(8);
+
+		persons[2] = new Person();
+		persons[2].setFirstName("Jane");
+		persons[2].setLastName("Graff");
+		persons[2].setAge(16);
+
+		persons[3] = new Person();
+		persons[3].setFirstName("Nancy");
+		persons[3].setLastName("Goodyear");
+		persons[3].setAge(69);
+
+		System.out.println("Print in Insertion Order");
+		for (int i = 0; i < 4; i++) {
+			Person person = persons[i];
+			String lastName = person.getLastName();
+			String firstName = person.getFirstName();
+			int age = person.getAge();
+			System.out.println(lastName + ", " + firstName + ". Age:" + age);
+		}
+
+		/* Example of using the Comparable interface in sorted data structure */
+		Set<Person> personSet = new TreeSet<Person>();
+		for (Person p : persons) {
+			personSet.add(p);
+		}
+
+		// print treeSet using Iterator objects
+		System.out.println();
+		System.out.println("Print data in Tree");
+		Iterator<Person> it = personSet.iterator();
+		while (it.hasNext()) {
+			Person person = it.next();
+			System.out.println(person);
+
+		}
+		
+		// calling a static inner class - static hence use new ClassName.constructor
+		Arrays.sort(persons, new Person.LastNameComparator());
+		System.out.println();
+		System.out.println("Sorted by last name");
+
+		for (int i = 0; i < 4; i++) {
+			Person person = persons[i];
+			String lastName = person.getLastName();
+			String firstName = person.getFirstName();
+			int age = person.getAge();
+			System.out.println(lastName + ", " + firstName + ". Age:" + age);
+		}
+
+		Arrays.sort(persons, new Person.FirstNameComparator());
+		System.out.println();
+		System.out.println("Sorted by first name");
+
+		for (int i = 0; i < 4; i++) {
+			Person person = persons[i];
+			System.out.println(person);
+		}
+		
+		
+
+		System.out.println();
+		Arrays.sort(persons); // uses the Comparable interface and overridden
+								// compareTo()
+		System.out.println("Sorted by age");
+
+		for (Person person : persons) {
+			System.out.println(person);
+		}
+	}
+}
