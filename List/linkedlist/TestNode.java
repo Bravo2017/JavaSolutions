86bb8de4c9d2389d00f9209775adb5d59544edcf
tanmay <tanmayss@gmail.com>
2015-08-06 07:41:53 -0400

    ni change

--- List/linkedlist/TestNode.java
+++ List/linkedlist/TestNode.java
@@ -5,8 +5,6 @@ public class TestNode {
 	public static void main(String[] args) {
 		LinkedList<String> list =  new LinkedList<String>();
 		
-//		list.head = list.createCircularList();
-//		System.out.println(	list.testCircular());
 		list.add("ta");
 		list.add("im");
 		list.add("ay");
@@ -15,15 +13,9 @@ public class TestNode {
 		list.add("ra");
 		list.add("im");
 		
-		
 		System.out.println(list);
-		list.removeDuplicateFromUnsortedList2();
+		list.partitionList("ch");
 		System.out.println(list);
-		
-//		list.returnNthToLastElement(2);
-		
-//		list.partitionList("ay");
-//		System.out.println(list);
 		
 	}
 
